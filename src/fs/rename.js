c9f419b54fa9b8c114dc6db8d68302cd7d930119
irina-mokh <75081244+irina-mokh@users.noreply.github.com>
2024-01-29 12:43:33 +0300

    fix: handle errors for rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,4 +1,4 @@
-import fs from 'fs/promises';
+import fs, {access} from 'fs/promises';
 import path from 'path';
 import { getDir } from '../utils.js';
 
@@ -6,10 +6,14 @@ const wrong = path.join(getDir(import.meta.url), 'files','wrongFilename.txt');
 const proper = path.join(getDir(import.meta.url), 'files','properFilename.md');
 
 const rename = async () => {
+  access(proper).then(()=>{
+		throw new Error('FS operation failed: file already exists')
+	})
+
     try {
         await fs.rename( wrong, proper);
 	} catch {
-  	    throw new Error('FS operation failed');
+  	    throw new Error('FS operation failed: file to rename not found');
 	}
 };
 
