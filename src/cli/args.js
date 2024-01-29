9bfa9a22d261ed3e1b61f827c0ae354c30dac9b3
irina-mokh <75081244+irina-mokh@users.noreply.github.com>
2024-01-29 13:16:09 +0300

    feat: handle wrong format

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,12 +1,18 @@
 import { argv } from 'process';
 
 const parseArgs = () => {
-    argv.forEach((val, i) => {
+    const res = [];
+    const args = argv.slice(2);
+    args.forEach((val, i) => {
         if (i % 2 === 0) {
-            const res = val.slice(2) + ' is ' + argv[i + 1] + '; ';
-            console.log(res);
+            if (val.startsWith('--')){
+                res.push(val.slice(2) + ' is ' + args[i + 1]);
+            } else {
+                throw new Error('Make sure your format is: --propName value --prop2Name value2');
+            }
         }
       });
+      console.log( res.length  ? res.join(', '): 'No args');
 };
 
 parseArgs();
